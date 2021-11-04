7927f373dc69b5217c938b616002de72592adec2
Alexander Ignition <izh.sever@gmail.com>
2021-11-04 21:06:46 +0300

    catbird 0.8.0

--- catbird.rb
+++ catbird.rb
@@ -1,8 +1,8 @@
 class Catbird < Formula
   desc "Mock server for UI tests"
   homepage "https://github.com/RedMadRobot/catbird"
-  url "https://github.com/RedMadRobot/catbird/releases/download/0.8.1/catbird.zip"
-  sha256 "7356e7851a20723a7c84438dfbe246415674a04f7d58bf114b6fc870b3ed1a70"
+  url "https://github.com/RedMadRobot/catbird/releases/download/0.8.0/catbird.zip"
+  sha256 "a1d6c25da916c9e5336571e69137134a6f8c0c10d2ba05fc917e70531f7b72cd"
 
   bottle :unneeded
 
