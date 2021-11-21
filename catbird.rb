a915ceca78e7b6cc4c595fdb904866d73beeaa94
runner <runner@Mac-1637482066232.local>
2021-11-21 08:24:07 +0000

    Bump version catbird 0.9.0

--- catbird.rb
+++ catbird.rb
@@ -1,8 +1,8 @@
 class Catbird < Formula
   desc "Mock server for UI tests"
   homepage "https://github.com/RedMadRobot/catbird"
-  url "https://github.com/RedMadRobot/catbird/releases/download/0.8.2/catbird.zip"
-  sha256 "9d3a468c71a64f43c839c7918f158827d9caa25191065299ffa05147cfe48957"
+  url "https://github.com/RedMadRobot/catbird/releases/download/0.9.0/catbird.zip"
+  sha256 "063a57cb98566f4d69c09cef9898b1461e962729c5e55b16110b60f5120a25fd"
 
   bottle :unneeded
 
